899456a9c7da43f7e5ac035dbfd691115a09d6cf
morteza rasooli <motteza1182@gmail.com>
2025-02-27 15:50:37 +0330

    make base in main file

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT  = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
+window = Tk()
+window.title('snake game')
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+# label = Label(window, text="score:{}".format(score), font=('consolas', 40))
+
+
+
+
+
+
+window.mainloop()
+
