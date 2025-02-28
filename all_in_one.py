fc0a87dba0adcce6edacbc6e212547ecc1693a1a
morteza rasooli <motteza1182@gmail.com>
2025-02-28 13:04:06 +0330

    make all in on file to ez job

---
+++ all_in_one.py
@@ -0,0 +1,93 @@
+from tkinter import *
+import random
+
+class Snake:
+    def __init__(self):
+        self.__body_size__ = BODY_PARTS
+        self.coordinates = list()
+        self.squares = list()
+        
+        for i in range(self.__body_size__):
+            self.coordinates.append([0, 0])
+            
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR ,tag='snake')
+
+class Food:
+    def __init__(self):
+        x = random.randint(0,int((GAME_WIDTH / SPACE_SIZE)-1)) * SPACE_SIZE
+        y = random.randint(0,int((GAME_HEIGHT / SPACE_SIZE)-1)) * SPACE_SIZE
+        self.coordonates = [x, y]
+        canvas.create_oval(self.coordonates[0], self.coordonates[1], self.coordonates[0] + SPACE_SIZE, self.coordonates[1] + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+def next_turn(snake, food):
+    x, y = snake.coordinates[0]
+    
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+        
+    snake.coordinates.insert(0, (x, y))
+    
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+    
+    snake.squares.insert(0, square)
+        
+    window.after(SPEED, next_turn, snake, food)
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+
+GAME_WIDTH = 700
+GAME_HEIGHT  = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+window = Tk()
+window.title('snake game')
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+# set window in center screen
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+next_turn(snake=snake, food=food)
+
+
+window.mainloop()
