5d0f5364e9f7ff556959a7de499d98bfa8920608
morteza rasooli <motteza1182@gmail.com>
2025-03-18 12:25:30 +0330

    make init in snake_class

---
+++ my_tern/snake_class.py
@@ -0,0 +1,24 @@
+import tkinter
+import json
+import os
+
+
+class Snake:
+    def __init__(self, canvas: tkinter.Canvas):
+        self.squares = list()
+        self.coordinates = list()
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        data_path = os.path.join(current_dir, "data.json")
+        with open(data_path, 'r') as f:
+            data = json.load(f)
+            self.snake_color = data["snake"]["snake_color"]
+            self.snake_size = data["snake"]["snake_size"]
+            self.pixel_size = data["canvas"]["pixel_size"]
+        
+        for i in range(self.snake_size):
+            self.coordinates.append([0,0])
+            
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x+self.pixel_size, y+self.pixel_size, fill=self.snake_color)
+            self.squares.append(square)
+        
