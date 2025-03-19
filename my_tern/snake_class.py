5115a4da4124cef29cd9da7a60e53bf18ce20dee
MR surface <motteza1182@gmail.com>
2025-03-19 15:00:26 +0330

    fix snake position

--- my_tern/snake_class.py
+++ my_tern/snake_class.py
@@ -14,9 +14,10 @@ class Snake:
             self.snake_color = data["snake"]["snake_color"]
             self.snake_size = data["snake"]["snake_size"]
             self.pixel_size = data["canvas"]["pixel_size"]
+            self.snake_position = data["snake"]["snake_position"]
         
         for i in range(self.snake_size):
-            self.coordinates.append([0,0])
+            self.coordinates.append([self.pixel_size*self.snake_position[0], self.pixel_size*self.snake_position[1]])
             
         for x, y in self.coordinates:
             square = canvas.create_rectangle(x, y, x+self.pixel_size, y+self.pixel_size, fill=self.snake_color)
