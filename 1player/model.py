065891b804c75c2e254c04a50e49278471028530
MR surface <motteza1182@gmail.com>
2025-04-15 12:05:54 +0330

    add df methods to Model class (its not test)

--- 1player/model.py
+++ 1player/model.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn 
 import torch.optim as optim
 import os
-import csv
+import pandas as pd
+import numpy as np
 
 class Model(nn.Module):
     def __init__(self):
@@ -10,38 +11,21 @@ class Model(nn.Module):
         self.line1 = nn.Linear(4, 16)
         self.line2 = nn.Linear(16, 1)
     
-    def update_model(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        data_path = os.path.join(current_dir, "data_set_snake_game.csv")
-        with open(data_path, 'r') as f:
-            x = []
-            y = []
-            reader = csv.reader(f)
-            for row in reader:
-                temp = row[0]
-                temp = temp[2:-2]
-                temp = temp.split(',')
-                temp = [int(temp[0]), int(temp[1])]
-                row_data = [temp[0], temp[1]]
-                temp = row[1]
-                temp = temp[2:-2]
-                temp = temp.split(',')
-                temp = [int(temp[0]), int(temp[1])]
-                row_data.append(temp[0])
-                row_data.append(temp[1])
-                x.append(row_data)
-                y.append(row[2])
-            self.x = torch.tensor(x)
-            self.y = torch.tensor(y)
+    def set_df(self):
+        self.df = self.__make_df()
+        self.df = self.__hot_one_encode(columns=['direction'])
+        self.__check_and_update_distance(self.df)
     
     def learning_loop(self, model:nn.Module):
+        x = torch.tensor(self.x)
+        y = torch.tensor(self.y)
         loss = nn.MSELoss()
         opt = optim.Adam(model.parameters(), lr=1e-3)
 
         for t in range(2000):
-            y_pred = model(self.x) # its mean model.forward(x), in pytorch we can write this model(x)
+            y_pred = model(x) # its mean model.forward(x), in pytorch we can write this model(x)
 
-            loss_t = loss(y_pred, self.y)
+            loss_t = loss(y_pred, y)
             if t % 100 == 99:
                 print(t, loss_t.item())
             
@@ -52,9 +36,30 @@ class Model(nn.Module):
             # update weght
             opt.step()
 
-
     def forward(self, x):
         x = torch.relu(self.line1(x))
         x = self.line2(x)
 
+    # private methods:
+
+    def __make_df(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        data_path = os.path.join(current_dir, "data_set_snake_game.csv")
+        df = pd.read_csv(data_path)
+        return df
+
+    def __hot_one_encode(self, columns):
+        return pd.get_dummies(self.df, columns=columns)
+    
+    def __check_and_update_distance(self):
+        distans = []
+        current_distance = 1e+4
+        for i in range(1,len(self.df)):
+            
+            next_distance = int(np.sqrt(np.pow(self.df.loc[i]['head_x'] - self.df.loc[i]['food_x'],2) + np.pow(self.df.loc[i]['head_y'] - self.df.loc[i]['food_y'],2)))
+            if current_distance < next_distance:
+                # print("current_distance: %s, next_distance: %s" %(current_distance, next_distance))
+                self.df = self.df.drop(index=i-1)
+            current_distance = next_distance
+        self.df.reset_index()
     
